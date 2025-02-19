d06ed99f0def0774c47a368f2083d8d64642cbf7
ostriftis <165506270+ostriftis@users.noreply.github.com>
2025-02-19 16:02:46 +0200

    added gpiow, gpior

--- AVR-OS.X/command_execution.c
+++ AVR-OS.X/command_execution.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <avr/eeprom.h>
+#include <stdlib.h>
 
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
@@ -19,21 +20,22 @@
 void parse_command(char *cmd)
 {
     char *cmd_name = strtok(cmd, " ");  // First token is the command name
-    char *arg = strtok(NULL, " ");      // Second token is the argument (if any)
-
+    char *arg[2] = {NULL, NULL};  // Allocate space for two arguments, initialized to NULL
+    arg[0] = strtok(NULL, " ");      // Second token is the argument (if any)
+    arg[1] = strtok(NULL, " ");      // Third token is the 2nd argument (only for gpiow)
     execute_command(cmd_name, arg);
 }
 
 // Process received commands
-void execute_command(char *cmd, char *arg)
+void execute_command(char *cmd, char **arg)
 {
     char readBuffer[50];
     if (strncmp(cmd, "setuser ", 4) == 0)
     {
         usart_send_string("Setting user: ");
-        usart_send_string(arg);
+        usart_send_string(arg[0]);
         usart_send_string("\n");
-        write_eeprom(0x00, arg);
+        write_eeprom(0x00, arg[0]);
         usart_send_string("User set successfully\n");
     }
     
@@ -45,7 +47,7 @@ void execute_command(char *cmd, char *arg)
     
     else if (strncmp(cmd, "setpass", 4) == 0)
     {
-        write_eeprom(0x20, arg);
+        write_eeprom(0x20, arg[0]);
         usart_send_string("Password set successfully\n");
     }
     
@@ -54,7 +56,76 @@ void execute_command(char *cmd, char *arg)
         read_eeprom(0x20, readBuffer, sizeof(readBuffer));
         usart_send_string(readBuffer);
     }
-
+    
+    else if (strcmp(cmd, "gpiow") == 0)
+    {   
+        uint8_t input;
+        char *value = arg[1];
+        if(value == NULL) input = 0x00;
+        else {
+            if (value[0] != '0' || value[1] != 'x' || value[2] == '\0') {
+                usart_send_string("Error: Invalid format. Expected 0xHH\n");
+            }
+            else {
+                input = (uint8_t) strtoul(value, NULL, 16);
+            }
+        }
+        
+        if(arg[0] != NULL) {
+            /*Check which port is to write to*/
+            if(strcmp(arg[0], "PORTB") == 0) {
+                PORTB = input;
+            }
+            else if(strcmp(arg[0], "PORTC") == 0) {
+                PORTC = input;
+            }
+            else if(strcmp(arg[0], "PORTD") == 0) {
+                PORTD = input;
+            }
+            else {/*Wrong port*/
+                usart_send_string("Invalid port\n");
+            }
+        }
+        else {
+            /*If argument is vacant we tell the user to provide it*/
+            usart_send_string("Port needs to be specified\n");
+        }
+    }
+    
+    else if(strcmp(cmd, "gpior") == 0)
+    {
+        uint8_t output = 0x00;
+        char printable[20];
+        if(arg[0] != NULL) {
+            /*Check which port is to write to*/
+            if(strcmp(arg[0], "PORTB") == 0) {
+                output = PORTB;
+                sprintf(printable, "PORTB = %d\n", output);
+                usart_send_string(printable);
+            }
+            else if(strcmp(arg[0], "PORTC") == 0) {
+                output = PORTC;
+                sprintf(printable, "PORTC = %d\n", output);
+                usart_send_string(printable);
+            }
+            else if(strcmp(arg[0], "PORTD") == 0) {
+                output = PORTD;
+                sprintf(printable, "PORTD = %d\n", output);
+                usart_send_string(printable);
+            }
+            else {/*Wrong port*/
+                usart_send_string("Invalid port\n");
+            }
+        }
+        else {
+            /*If argument is vacant we tell the user to provide it*/
+            usart_send_string("Port needs to be specified\n");
+        }
+    }
+    else {
+        usart_send_string("Uknown command\n");
+    }
+    return;
 }
 
 void login()
