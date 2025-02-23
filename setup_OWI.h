7db306aa2947070d2097146d9d5b62749c97d7bd
ostriftis <165506270+ostriftis@users.noreply.github.com>
2025-02-24 00:49:43 +0200

    Delete setup_OWI.h

--- setup_OWI.h
+++
@@ -1,5 +0,0 @@
-uint8_t one_wire_reset();
-uint8_t one_wire_receive_bit();
-void one_wire_transmit_bit(uint8_t output);
-uint8_t one_wire_receive_byte();
-void one_wire_transmit_byte(uint8_t output);
