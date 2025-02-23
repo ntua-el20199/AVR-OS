7d84b10d1610c693bea44b2fcb00dfb6673ee0b6
ostriftis <165506270+ostriftis@users.noreply.github.com>
2025-02-24 00:49:03 +0200

    Add files via upload

---
+++ setup_OWI.h
@@ -0,0 +1,5 @@
+uint8_t one_wire_reset();
+uint8_t one_wire_receive_bit();
+void one_wire_transmit_bit(uint8_t output);
+uint8_t one_wire_receive_byte();
+void one_wire_transmit_byte(uint8_t output);
